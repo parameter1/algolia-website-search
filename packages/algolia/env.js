391499f2a6c37726faf84b09576489aeb209b6eb
Jacob Bare <jacob@parameter1.com>
2021-01-07 10:57:57 -0500

    Create algola client package

---
+++ packages/algolia/env.js
@@ -0,0 +1,9 @@
+const {
+  cleanEnv,
+  str,
+} = require('envalid');
+
+module.exports = cleanEnv(process.env, {
+  ALGOLIA_APP_ID: str({ desc: 'The Algolia website search application ID', default: '05JLREVGZ4' }),
+  ALGOLIA_API_KEY: str({ desc: 'The Algolia website search API key (must be writeable)' }),
+});

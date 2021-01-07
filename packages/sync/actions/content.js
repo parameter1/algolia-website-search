ad2a8c1c65173015d1e612b8eafc47dd2365cd7b
Jacob Bare <jacob@parameter1.com>
2021-01-07 12:52:30 -0500

    Add content sync projection

--- packages/sync/actions/content.js
+++ packages/sync/actions/content.js
@@ -3,6 +3,25 @@ const transform = require('@algolia-website-search/transformers');
 
 const getIndexFor = ({ tenant, algolia }) => algolia.initIndex(`${tenant}_platform_content`);
 
+const projection = {
+  type: 1,
+  name: 1,
+  fullName: 1,
+  status: 1,
+  body: 1,
+  teaser: 1,
+  published: 1,
+  unpublished: 1,
+  created: 1,
+  updated: 1,
+  'mutations.Website.primarySite': 1,
+  'mutations.Website.primarySection': 1,
+  'mutations.Website.name': 1,
+  'mutations.Website.body': 1,
+  'mutations.Website.teaser': 1,
+  sectionQuery: 1,
+};
+
 module.exports = {
   /**
    *
@@ -19,7 +38,9 @@ module.exports = {
   /**
    *
    */
-  saveAll: async () => {},
+  saveAll: async () => {
+
+  },
 
   /**
    *
@@ -34,7 +55,7 @@ module.exports = {
       id: Joi.number().required(),
     }).validateAsync(params);
     const index = getIndexFor({ tenant, algolia });
-    const doc = await repos.platformContent.findById({ id, options: { strict: true } });
+    const doc = await repos.platformContent.findById({ id, options: { strict: true, projection } });
     const object = await transform('platform.content', { doc }, { dataloaders });
     return index.saveObject(object);
   },

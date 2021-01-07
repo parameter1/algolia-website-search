71cd86393bf8537e5612c69fc4478394240dd83a
Jacob Bare <jacob@parameter1.com>
2021-01-07 11:34:12 -0500

    Add content deleteOne and saveOne actions

--- packages/sync/actions/content.js
+++ packages/sync/actions/content.js
@@ -1,5 +1,30 @@
+const Joi = require('@parameter1/joi');
+const transform = require('@algolia-website-search/transformers');
+
+const getIndexFor = ({ tenant, algolia }) => algolia.initIndex(`${tenant}_platform_content`);
+
 module.exports = {
-  deleteOne: async () => {},
-  saveMany: async () => {},
-  saveOne: async () => {},
+  deleteOne: async (params = {}, { tenant, algolia }) => {
+    const { id } = await Joi.object({
+      id: Joi.number().required(),
+    }).validateAsync(params);
+
+    const index = getIndexFor({ tenant, algolia });
+    return index.deleteObject(`${id}`);
+  },
+  saveAll: async () => {},
+  saveOne: async (params = {}, {
+    algolia,
+    dataloaders,
+    repos,
+    tenant,
+  }) => {
+    const { id } = await Joi.object({
+      id: Joi.number().required(),
+    }).validateAsync(params);
+    const index = getIndexFor({ tenant, algolia });
+    const doc = await repos.platformContent.findById({ id, options: { strict: true } });
+    const object = await transform('platform.content', { doc }, { dataloaders });
+    return index.saveObject(object);
+  },
 };

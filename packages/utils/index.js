a6b328cdcb073278bf8b015e337c3f5c955ee5bf
Jacob Bare <jacob@parameter1.com>
2021-01-07 10:03:07 -0500

    Create batch utility

--- packages/utils/index.js
+++ packages/utils/index.js
@@ -1,7 +1,9 @@
+const batch = require('./batch');
 const dateToUNIX = require('./date-to-unix');
 const objectPath = require('./object-path');
 
 module.exports = {
+  batch,
   dateToUNIX,
   objectPath,
 };

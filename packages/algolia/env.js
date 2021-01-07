30711901bbf46d79fe9722c6029c00340bea8519
Jacob Bare <jacob@parameter1.com>
2021-01-07 11:11:53 -0500

    Remove algolia package

--- packages/algolia/env.js
+++
@@ -1,9 +0,0 @@
-const {
-  cleanEnv,
-  str,
-} = require('envalid');
-
-module.exports = cleanEnv(process.env, {
-  ALGOLIA_APP_ID: str({ desc: 'The Algolia website search application ID', default: '05JLREVGZ4' }),
-  ALGOLIA_API_KEY: str({ desc: 'The Algolia website search API key (must be writeable)' }),
-});

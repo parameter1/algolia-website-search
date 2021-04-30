f44241f7b418d7941cbf814b483fa0bb701a02bd
Jacob Bare <jacob@parameter1.com>
2021-04-30 12:13:51 -0500

    Fix path import lint error

--- packages/sync/mongodb.js
+++ packages/sync/mongodb.js
@@ -1,6 +1,6 @@
 const MongoDBClient = require('@parameter1/mongodb/client');
-const { MONGO_URI } = require('./env');
 const { join } = require('path');
+const { MONGO_URI } = require('./env');
 const { name, version } = require(join(process.cwd(), 'package.json'));
 
 module.exports = new MongoDBClient({

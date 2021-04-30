b8acc509c0739a6040b40afb7263c6820ed34c44
Jacob Bare <jacob@parameter1.com>
2021-04-29 21:10:11 -0500

    Include AWS_EXECUTION_ENV in env
    Required for envalid v7

--- lambda/env.js
+++ lambda/env.js
@@ -5,5 +5,6 @@ const {
 
 module.exports = cleanEnv(process.env, {
   ALGOLIA_API_KEY: str({ desc: 'The Algolia website search API key (must be writeable)' }),
+  AWS_EXECUTION_ENV: str({ desc: 'The AWS lambda environment. Only provided when running in the cloud.' }),
   MONGO_URI: str({ desc: 'The BaseCMS MongoDB instance to connect to.' }),
 });

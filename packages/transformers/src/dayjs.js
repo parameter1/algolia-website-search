1632a91abf8115f9552f24dee36ce5f50ad7367f
Jacob Bare <jacob@parameter1.com>
2021-04-29 20:58:38 -0500

    Remove dayjs

--- packages/transformers/src/dayjs.js
+++
@@ -1,7 +0,0 @@
-const dayjs = require('dayjs');
-const utc = require('dayjs/plugin/utc');
-const timezone = require('dayjs/plugin/timezone');
-
-module.exports = dayjs
-  .extend(utc)
-  .extend(timezone);
